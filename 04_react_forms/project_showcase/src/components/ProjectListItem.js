c1e8340bc390d69ea0f74fd67ba4b426eab431e2
stephenlambertfs <stephenolambert@gmail.com>
2023-07-07 10:41:16 -0600

    added forms lecture

---
+++ 04_react_forms/project_showcase/src/components/ProjectListItem.js
@@ -0,0 +1,33 @@
+import React, {useState} from "react"
+const ProjectListItem = ({ id, about, image, link, name, phase }) => {
+  const [count, setCount] = useState(0)
+
+  function handleClick(){
+    // setCount((claps) => claps + 1)
+    setCount(count + 1)
+  }
+  return (
+    <li className="card">
+      <figure className="image">
+        <img src={image} alt={name} />
+        <button className="claps" onClick={handleClick}>👏{count}</button>
+      </figure>
+
+      <section className="details">
+        <h4>{name}</h4>
+        <p>{about}</p>
+        {link ? (
+          <p>
+            <a href={link}>Link</a>
+          </p>
+        ) : null}
+      </section>
+
+      <footer className="extra">
+        <span className="badge blue">Phase {phase}</span>
+      </footer>
+    </li>
+  );
+};
+
+export default ProjectListItem;
