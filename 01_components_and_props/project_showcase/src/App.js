5ea15a9029a77e955c6377c1c447722311901516
stephenlambertfs <stephenolambert@gmail.com>
2023-09-29 15:46:49 -0600

    updated starter code

--- 01_components_and_props/project_showcase/src/App.js
+++ 01_components_and_props/project_showcase/src/App.js
@@ -1,16 +1,5 @@
-import Header from "./components/Header"
-import ProjectForm from "./components/ProjectForm"
-import ProjectList from "./components/ProjectList"
-import projects from "./projects"
 function App() {
-  console.log(projects)
-  return (
-  <div className="App">
-    <Header />
-    <ProjectForm />
-    <ProjectList projects = {projects}/>
-   </div>
-  );
+  return <div className="App">Project showcase</div>;
 }
 
 export default App;
